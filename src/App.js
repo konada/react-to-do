6b9ed9e005701f0ba4c8cb0efd498b4758186c33
konada <boards.of.konada@gmail.com>
2017-10-04 21:36:03 +0200

    update main app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Item from './Item/Item';
+import ListForm from './List/ListForm';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.addItem = this.addItem.bind(this);
+
+    this.state ={
+      items: [
+        { id: 1, itemContent: "Item 1" },
+     
+      ],
+    }
+  }
+
+  addItem(item){
+    const previousItems = this.state.items;
+    previousItems.push({itemContent: item});
+    this.setState({
+      items: previousItems
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to TO-DO-LIST</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+     <div className="itemsWrapper">
+      <div className="itemsHeader">
+        <div className="heading">ReactJS TO-DO-LIST</div>
       </div>
+      <div className="itemsBody">
+        {
+          this.state.items.map((item) => {
+            return(
+            <Item itemContent={item.itemContent} itemId={item.id} key={item.id}/>
+            )
+          })
+        }
+      </div>
+      <div className="itemsFooter">
+          <ListForm addItem={this.addItem}/>    
+      </div>
+    </div>
+     
+     
     );
   }
 }
